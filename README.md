555722e2aacba57a4d5baee94f80bd35c1a035d0
Ryan Tan <languantan@gmail.com>
2019-02-14 11:02:41 +0800

    Update README.md to add how-to - mv karabiner.json to folder - update install.sh to install asdf and neovim

--- README.md
+++ README.md
@@ -8,13 +8,17 @@
 - LastPass
 - Docker for Mac
 
+## How-to
+`sh install.sh` - will install the stuff below
+> Need to install VIM Plugins yourself
+
 ## Command line installs
 
-- Install brew
-- `brew install shiftit`
-- `brew install yarn`
-- Install neovim
-- Install asdf
+- Brew - MacOS package manager
+- Shiftit - Window organizer
+- Yarn - node package manager
+- neovim - VIM editor with advanced features
+- asdf - Version Manager for languages + frameworks, replaces nvm, rvm etc
   - node
   - ruby
   - python
--- install.sh
+++ install.sh
@@ -5,15 +5,24 @@
 
 #### install helpers
 brew cask install shiftit
+brew cask install karabiner-elements
 brew install yarn
 brew install ack
+brew install neovim
+
+#### install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.3
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
 
 #### Aliases
 cat .aliases >> ~/.aliases
 
 #### set up nvim
-cat init.vim > ~/.config/nvim/init.vim
+cat neovim/init.vim > ~/.config/nvim/init.vim
 
 #### set up tmuxinator
 cp .tmux.conf ~/.tmux.conf
 
+#### copy key bindings for ESC key
+cp karabiner/karabiner.json ~/.config/karabiner/
